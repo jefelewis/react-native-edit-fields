9b48f841cff613e8a642468441daa2919e50cbe6
Jeff Lewis <jeffylew77@gmail.com>
2020-02-06 15:54:44 -0800

    [Dev] Adding EditListField

---
+++ src/EditListField.tsx
@@ -0,0 +1,282 @@
+// Imports: Dependencies
+import React, { useState } from 'react';
+import { Button, Dimensions, Platform, Picker, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Modal from 'react-native-modal';
+
+// Screen Dimensions
+const { height, width } = Dimensions.get('window');
+
+// TypeScript: Types
+interface Props {
+  title?: string;
+  items: Array<Item>;
+  onChange: (item: any) => any;
+}
+
+interface Item {
+  label: string;
+  value: string;
+  key: number | string;
+};
+
+// Component: Edit List Field
+const EditListField = (props: Props) => {
+  // React Hooks: State
+  const [ modalVisible, toggle ] = useState(false);
+  const [ tempItem, setTempItem ] = useState();
+  const [ item, setItem ] = useState();
+
+  // Toggle Modal
+  const toggleModal = () => {
+    try {
+      // Check Platform (iOS)
+      if (Platform.OS === 'ios') {
+        // React Hook: Toggle Modal
+        toggle((modalVisible: boolean) => !modalVisible);
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Select Item
+  const selectItem = (item: string) => {
+    try {
+      // Check Platform (iOS)
+      if (Platform.OS === 'ios') {
+        // React Hook: Set Temp State
+        setTempItem(item);
+      }
+
+      // Check Platform (Android)
+      else if (Platform.OS === 'android') {
+        // React Hook: Set Item
+        setItem(item);
+
+        // React Props: onChange
+        props.onChange(item);
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Render iOS Picker
+  const renderIOSPicker = (props: Props) => {
+    try {
+      return (
+        <Picker
+          selectedValue={tempItem !== undefined ? tempItem : item}
+          onValueChange={(item) => selectItem(item)}>
+          {props.items.map((item: Item) => {
+            return (
+              <Picker.Item
+                label={item.label}
+                value={item.value}
+                key={item.key || item.label}
+              />
+            );
+          })}
+        </Picker>
+      )
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Press Cancel
+  const pressCancel = () => {
+    try {
+      // Set Temp Item
+      setTempItem(item);
+
+      // Toggle Modal
+      toggleModal();
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Press Done
+  const pressDone = () => {
+    try {
+      // React Hook: Set Item
+      setItem(tempItem);
+
+      // Props: onChange
+      props.onChange(tempItem);
+
+      // Toggle Modal
+      toggleModal();
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Render Platform
+  const renderPlatform = () => {
+    try {
+      // Check Platform (iOS)
+      if (Platform.OS === 'ios') {
+        return (
+          <View style={styles.container}>
+            <View style={styles.fieldTitleContainer}>
+              <Text style={styles.fieldTitle} numberOfLines={1}>{props.title === undefined ? 'List' : props.title}</Text>
+            </View>
+
+            <TouchableOpacity onPress={() => toggleModal()} style={styles.fieldTextContainer}>
+              <Text style={styles.fieldText} numberOfLines={1}>{item !== undefined ? item : 'Select'}</Text>
+            </TouchableOpacity>
+
+            <Modal
+              isVisible={modalVisible}
+              style={styles.modal}
+              backdropOpacity={.30}
+            >
+              <View style={styles.modalContainer}>
+                <View style={styles.pickerHeaderContainer}>
+                  <TouchableOpacity onPress={() => pressCancel()} >
+                    <Text style={styles.cancelText}>Cancel</Text>
+                  </TouchableOpacity>
+
+                  <View style={styles.doneButton}>
+                    <Button
+                      onPress={() => pressDone()}
+                      title="Done"
+                      disabled={item === tempItem ? true : false}
+                    />
+                  </View>
+                </View>
+
+                <View style={styles.pickerContainer}>
+                  {renderIOSPicker(props)}
+                </View>
+              </View>
+            </Modal>
+          </View>
+        );
+      }
+
+      // Check Platform (Android)
+      if (Platform.OS === 'android') {
+        return (
+          <View style={styles.container}>
+            <View style={styles.fieldTitleContainer}>
+              <Text style={styles.fieldTitle}>{props.title}</Text>
+            </View>
+
+            <View style={styles.fieldTextContainer}>
+              <Picker
+                selectedValue={item}
+                style={{height: 60, width: width - 16}}
+                onValueChange={selectItem}
+                mode="dropdown"
+              >
+                {props.items.map((item: Item) => {
+                  return (
+                    <Picker.Item
+                      label={item.label}
+                      value={item.value}
+                      key={item.key || item.label}
+                    />
+                  );
+                })}
+              </Picker>
+            </View>
+          </View>
+        )
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <View>
+      {renderPlatform()}
+    </View>
+  )
+};
+
+// Styles
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    width: width - 16,
+    height: 55,
+    borderColor: '#7D7D7D',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    marginLeft: 16,
+    marginBottom: 4,
+  },
+  fieldTitleContainer: {
+    width: 120,
+  },
+  fieldTitle: {
+    fontFamily: 'System',
+    fontSize: 17,
+    fontWeight: '600',
+    color: '#000000',
+  },
+  fieldTextContainer: {
+    width: width - 120 - 32,
+    justifyContent: 'flex-end',
+  },
+  fieldText: {
+    fontFamily: 'System',
+    fontSize: 17,
+    fontWeight: '400',
+    color: '#000000',
+  },
+  modal: {
+    margin: 0,
+  },
+  modalContainer: {
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  pickerHeaderContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 40,
+    width: width,
+    backgroundColor: '#FAFAF8',
+    borderColor: '#7D7D7D',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  pickerContainer: {
+    height: 250,
+    width: width,
+    backgroundColor: 'white',
+  },
+  doneButton: {
+    marginRight: 7,
+  },
+  doneText: {
+    fontFamily: 'System',
+    color: '#007AFF',
+    fontWeight: '600',
+    fontSize: 17,
+    marginRight: 16,
+  },
+  cancelText: {
+    fontFamily: 'System',
+    color: '#007AFF',
+    fontWeight: '400',
+    fontSize: 17,
+    marginLeft: 16,
+  },
+});
+
+// Exports
+export default EditListField;
