0a23544f04398fe6dffba7f02a4dafd4d5774c07
Jeff Lewis <jeffylew77@gmail.com>
2020-02-06 15:29:14 -0800

    [Dev] Adding EditDateTimeField

---
+++ src/EditDateTimeField.tsx
@@ -0,0 +1,245 @@
+// Imports: Dependencies
+import React, { useState, useEffect } from 'react';
+import { Button, Dimensions, Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import RNDateTimePicker from '@react-native-community/datetimepicker';
+import Modal from 'react-native-modal';
+import moment from 'moment';
+
+// Screen Dimensions
+const { height, width } = Dimensions.get('window');
+
+// TypeScript: Types
+interface Props {
+  title?: string;
+  onChange: (date: Date | string) => Date | string | void;
+}
+
+// Screen: Edit Date Time Field
+const EditDateTimeField = (props: Props) => {
+  // React Hooks: State
+  const [ modalVisible, toggle ] = useState(false);
+  const [ date, setDate ] = useState(new Date());
+  const [ tempDate, setTempDate ] = useState(date);
+  const [ today , todaySent ] = useState(false);
+
+  // React Hooks: Lifecycle Methods
+  useEffect(() => {
+    // Send Initial Date
+    if (today === false) {
+      // Props: onFromChange
+      props.onChange(new Date());
+
+      // Today's Date Has Been Sent To Parent Component
+      todaySent(true);
+    }
+  });
+
+  // Toggle Modal
+  const toggleModal = () => {
+    try {
+      // Check Platform (iOS)
+      if (Platform.OS === 'ios') {
+        // React Hook: Toggle Modal
+        toggle((modalVisible: boolean) => !modalVisible);
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Select Date
+  const selectDate = (event: any, newDate: Date) => {
+    try {
+      // React Hook: Set Temp State
+      setTempDate(newDate);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Render iOS Picker
+  const renderIOSPicker = () => {
+    try {
+      return (
+        <RNDateTimePicker
+          mode="datetime"
+          value={tempDate ? tempDate : date}
+          onChange={(event: any, newDate: any) => selectDate(event, newDate)}
+        />
+      )
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Press Cancel
+  const pressCancel = () => {
+    try {
+      // React Hook: Set Temp Date
+      setTempDate(date);
+
+      // Toggle Modal
+      toggleModal();
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Press Done
+  const pressDone = () => {
+    try {
+      // React Hook: Set Date
+      setDate(tempDate);
+
+      // Props: onChange
+      props.onChange(tempDate);
+
+      // Toggle Modal
+      toggleModal();
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Render Platform
+  const renderPlatform = () => {
+    try {
+      // Check Platform (iOS)
+      if (Platform.OS == 'ios') {
+        return (
+          <View style={styles.container}>
+            <View style={styles.fieldTitleContainer}>
+            <Text style={styles.fieldTitle} numberOfLines={1}>{props.title === undefined ? 'Date/Time' : props.title}</Text>
+            </View>
+
+            <TouchableOpacity onPress={() => toggleModal()} style={styles.fieldTextContainer}>
+              <Text style={styles.fieldText} numberOfLines={1}>{date ? moment(date).format('MMM Do, YYYY h:mm a') : 'Select'}</Text>
+            </TouchableOpacity>
+
+            <Modal
+              isVisible={modalVisible}
+              style={styles.modal}
+              backdropOpacity={.30}
+            >
+              <View style={styles.modalContainer}>
+                <View style={styles.pickerHeaderContainer}>
+                  <TouchableOpacity onPress={() => pressCancel()} >
+                    <Text style={styles.cancelText}>Cancel</Text>
+                  </TouchableOpacity>
+
+                  <View style={styles.doneButton}>
+                    <Button
+                      onPress={() => pressDone()}
+                      title="Done"
+                      disabled={date === tempDate ? true : false}
+                    />
+                  </View>
+                </View>
+
+                <View style={styles.pickerContainer}>
+                  {renderIOSPicker()}
+                </View>
+              </View>
+            </Modal>
+          </View>
+        );
+      }
+
+      // Check Platform (Android)
+      if (Platform.OS === 'android') {
+        return null;
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <View>
+      {renderPlatform()}
+    </View>
+  )
+};
+
+// Styles
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    width: width - 16,
+    height: 55,
+    borderColor: '#7D7D7D',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    marginLeft: 16,
+    marginBottom: 4,
+  },
+  fieldTitleContainer: {
+    width: 120,
+  },
+  fieldTitle: {
+    fontFamily: 'System',
+    fontSize: 17,
+    fontWeight: '600',
+    color: '#000000',
+  },
+  fieldTextContainer: {
+    width: width - 120 - 32,
+    justifyContent: 'flex-end',
+  },
+  fieldText: {
+    fontFamily: 'System',
+    fontSize: 17,
+    fontWeight: '400',
+    color: '#000000',
+  },
+  modal: {
+    margin: 0,
+  },
+  modalContainer: {
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  pickerHeaderContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 40,
+    width: width,
+    backgroundColor: '#FAFAF8',
+    borderColor: '#7D7D7D',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  pickerContainer: {
+    height: 250,
+    width: width,
+    backgroundColor: 'white',
+  },
+  doneButton: {
+    marginRight: 7,
+  },
+  doneText: {
+    fontFamily: 'System',
+    color: '#007AFF',
+    fontWeight: '600',
+    fontSize: 17,
+    marginRight: 16,
+  },
+  cancelText: {
+    fontFamily: 'System',
+    color: '#007AFF',
+    fontWeight: '400',
+    fontSize: 17,
+    marginLeft: 16,
+  },
+});
+
+// Exports
+export default EditDateTimeField;
